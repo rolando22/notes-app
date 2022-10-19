05371cc937bcdf672ba7a5f2edc8abf36a5e63c6
rolando22 <rolando2207@gmail.com>
2022-10-19 15:25:00 -0300

    [ADD] dirs layouts y partials [ADD] settings de express-handlebars

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const path = require('path');
 
 const app = express();
@@ -6,6 +7,13 @@ const app = express();
 //Setting
 app.set('port', process.env.PORT || 3001);
 app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs',
+}));
+app.set('view engine', '.hbs')
 
 //Middlewares
 app.use(express.urlencoded({ extended: false }));
