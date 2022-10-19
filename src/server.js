8bd3cc1eb742594a74e2034c1c9679fe47c760d0
rolando22 <rolando2207@gmail.com>
2022-10-19 15:38:15 -0300

    [ADD] hbs Main e Index

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const exphbs = require('express-handlebars');
+const { engine } = require('express-handlebars');
 const path = require('path');
 
 const app = express();
@@ -7,7 +7,7 @@ const app = express();
 //Setting
 app.set('port', process.env.PORT || 3001);
 app.set('views', path.join(__dirname, 'views'));
-app.engine('.hbs', exphbs({
+app.engine('.hbs', engine({
     defaultLayout: 'main',
     layoutsDir: path.join(app.get('views'), 'layouts'),
     partialsDir: path.join(app.get('views'), 'partials'),
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 
 //Routes
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.render('index');
 });
 
 //Static files
