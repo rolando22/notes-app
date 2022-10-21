01f7c846ae672d5b1743214810543aec359db1cb
rolando22 <rolando2207@gmail.com>
2022-10-20 21:42:32 -0300

    [ADD] persistencia de nuevas Notes en la DB y solicitud de Notes [ADD] maquetación y estilos de All-Notes

--- src/controllers/notes.controller.js
+++ src/controllers/notes.controller.js
@@ -1,13 +1,18 @@
+const Note = require('../models/Note');
+
 notesCtrl = {
     renderNoteForm: (req, res) => {
         res.render('notes/new-note');
     },
-    createNewNote: (req, res) => {
-        console.log(req.body);
+    createNewNote: async (req, res) => {
+        const { title, description } = req.body;
+        const newNote = new Note({ title, description });
+        await newNote.save();
         res.send('new note');
     },
-    renderNotes: (req, res) => {
-        res.send('render notes');
+    renderNotes: async (req, res) => {
+        const notes = await Note.find().lean();
+        res.render('notes/all-notes', { notes });
     },
     renderEditForm: (req, res) => {
         res.send('render edit form');
