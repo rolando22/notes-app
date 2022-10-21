ccf378bc215c89fb463f5130287d9b975580c500
rolando22 <rolando2207@gmail.com>
2022-10-21 15:20:44 -0300

    [ADD] funciones para editar Note [ADD] navegación para listar Notes y agregar Note

--- src/controllers/notes.controller.js
+++ src/controllers/notes.controller.js
@@ -14,11 +14,14 @@ notesCtrl = {
         const notes = await Note.find().lean();
         res.render('notes/all-notes', { notes });
     },
-    renderEditForm: (req, res) => {
-        res.send('render edit form');
+    renderEditForm: async (req, res) => {
+        const note = await Note.findById(req.params.id).lean();
+        res.render('notes/edit-note', { note });
     },
-    updateNote: (req, res) => {
-        res.send('update note');
+    updateNote: async (req, res) => {
+        const { title, description } = req.body;
+        await Note.findByIdAndUpdate(req.params.id, { title, description });
+        res.redirect('/notes');
     },
     deleteNote: async (req, res) => {
         await Note.findByIdAndDelete(req.params.id);
