0ba8afdcd16e8b7141b0f243758e825c92260c28
rolando22 <rolando2207@gmail.com>
2022-10-25 00:03:12 -0300

    [ADD] notas públicas a privadas

--- src/controllers/notes.controller.js
+++ src/controllers/notes.controller.js
@@ -7,15 +7,17 @@ const notesCtrl = {
     createNewNote: async (req, res) => {
         const { title, description } = req.body;
         const newNote = new Note({ title, description });
+        newNote.user = req.user._id;
         await newNote.save();
         res.redirect('/notes');
     },
     renderNotes: async (req, res) => {
-        const notes = await Note.find().lean();
+        const notes = await Note.find({ user: req.user._id }).sort({ createdAt: 'desc' }).lean();
         res.render('notes/all-notes', { notes });
     },
     renderEditForm: async (req, res) => {
         const note = await Note.findById(req.params.id).lean();
+        if (note.user !== req.user._id) return res.redirect('/notes', { text: 'Not authorized.' });
         res.render('notes/edit-note', { note });
     },
     updateNote: async (req, res) => {
